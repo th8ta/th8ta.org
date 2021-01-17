c80938c93add798df6817c190a12d3ad3bb86b5f
tate <tateflyer1@gmail.com>
2021-01-17 02:33:49 -0500

    feat: initialize refactor

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,13 @@ import Head from "next/head";
 import Metas from "../components/Metas";
 import Nav from "../components/Nav";
 import styles from "../styles/Home.module.sass";
+import { VertoProvider, Page, Card } from "@verto/ui";
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>th8ta</title>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.1.9/p5.js"></script>
-        <script src="https://www.openprocessing.org/openprocessing_sketch.js"></script>
-        <script src="/bg.js" />
       </Head>
       <Metas
         title="Th8ta"
@@ -18,11 +16,31 @@ export default function Home() {
         image="/favicon.png"
       />
       <Nav />
-      <div className={styles.Landing}>
-        <h1>
-          th<span>8</span>ta
-        </h1>
-      </div>
+      <VertoProvider>
+        <Page>
+          <h3>Building from all angles.</h3>
+          <div className={styles.Hero}></div>
+          <h1>Ventures</h1>
+          <Card
+            style={{ marginBottom: "1em" }}
+            className={styles.testToken}
+            hoverable
+            shadow
+            onClick={() => (window.location.href = "https://verto.exchange")}
+          >
+            <h1 className={styles.code}>VRT</h1>
+            <div className={styles.info}>
+              <h4 className={styles.code}>
+                <span>[PST]</span>Verto
+              </h4>
+              <p className={styles.code}>
+                <span>ID:</span>usjm4PCxUd5mtaon7zc97-dt-3qf67yPyqgzLnLqk5A
+              </p>
+            </div>
+          </Card>
+          <h1>About us</h1>
+        </Page>
+      </VertoProvider>
     </>
   );
 }
